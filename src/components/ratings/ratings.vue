646dced90b2c587476b453df154e0e4a86a77d8f
lianglunzhong <lianglunzhong@gmail.com>
2018-02-27 17:20:09 +0800

    updated:ratings

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="ratings">
+    <div class="ratings" ref="ratings">
       <div class="ratings-content">
         <div class="overview">
           <div class="overview-left">
@@ -24,18 +24,56 @@
             </div>
           </div>
         </div>
+        <v-split></v-split>
+        <v-ratingselect :select-type="selectType" :only-content="onlyContent" :ratings="ratings" :desc="desc"></v-ratingselect>
+        <div class="rating-wrapper">
+          <ul>
+            <li v-show="needShow(rating.rateType, rating.text)" v-for="(rating, index) in ratings" :key="index" class="rating-item">
+              <div class="avatar">
+                <img :src="rating.avatar" width="28" height="28">
+              </div>
+              <div class="content">
+                <h1 class="name">{{rating.username}}</h1>
+                <div class="star-wapper">
+                  <v-star :size="24" :score="rating.score"></v-star>
+                  <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}</span>
+                </div>
+                <p class="text">{{rating.text}}</p>
+                <div class="recommend" v-show="rating.recommend && rating.recommend.length">
+                  <span class="icon-thumb_up"></span>
+                  <span v-for="(item, index2) in rating.recommend" :key="index2" class="item">{{item}}</span>
+                </div>
+                <div class="time">{{rating.rateTime | formatDate}}</div>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
   import Star from '@/components/star/star';
+  import EventBus from '../../common/js/EventBus';
+  import formatDate from '../../common/js/date';
+  import BScroll from 'better-scroll';
+  import Split from '@/components/split/split';
+  import RatingSelect from '@/components/ratingselect/ratingselect';
+
+  const ALL = 2;
 
   export default {
     name: 'Ratings',
     data () {
       return {
-        msg: '我是ratings'
+        ratings: [],
+        selectType: ALL,
+        onlyContent: true,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
       };
     },
     props: {
@@ -43,8 +81,62 @@
         type: Object
       }
     },
+    created() {
+      this.$http.jsonp('http://127.0.0.1:8000/api/ratings',
+        {
+          params: {},
+          jsonp: 'callback'
+        }).then(function(response) {
+          if (response.body.status) {
+            this.ratings = response.body.data;
+            this.$nextTick(function() {
+              this.scroll = new BScroll(this.$refs.ratings, {
+                click: true
+              });
+            });
+          }
+        }, function(response) {
+          console.log(response);
+      });
+
+      let self = this;
+      EventBus.$on('ratingtype.select', function(type) {
+        self.selectType = type;
+        self.$nextTick(() => {
+          self.scroll.refresh();
+        });
+      });
+
+      EventBus.$on('content.toggle', function(onlyContent) {
+        self.onlyContent = onlyContent;
+        self.$nextTick(() => {
+          self.scroll.refresh();
+        });
+      });
+    },
+    methods: {
+      needShow(type, text) {
+        if (this.onlyContent && !text) {
+          return false;
+        }
+
+        if (this.selectType === ALL) {
+          return true;
+        } else {
+          return type === this.selectType;
+        }
+      }
+    },
+    filters: {
+      formatDate(time) {
+        let date = new Date(time);
+        return formatDate(date, 'yyyy-MM-dd hh:mm');
+      }
+    },
     components: {
-      'v-star': Star
+      'v-star': Star,
+      'v-split': Split,
+      'v-ratingselect': RatingSelect
     }
   };
 </script>
@@ -119,5 +211,63 @@
           .delivery
             margin-left: 12px
             font-size: 12px
+            color: rgb(147, 153, 159)
+    .rating-wrapper
+      padding: 0 18px
+      .rating-item
+        display: flex
+        padding: 18px 0
+        border-1px(rgba(7, 17, 27, 0.1))
+        .avatar
+          flex: 0 0 28px
+          width: 28px
+          margin-right: 12px
+          img
+            border-radius: 50%
+        .content
+          flex: 1
+          position: relative
+          .name
+            margin-bottom: 4px
+            line-heigth: 12px
+            font-size: 10px
+            color: rgb(7, 17, 27)
+          .star-wapper
+            margin-bottom: 6px
+            font-size: 0
+            .star
+              display: inline-block
+              margin-right: 6px
+              vertical-align: top
+            .delivery
+              line-heigth: 12px
+              font-size: 10px
+              color: rgb(147, 153, 159)
+          .text
+            margin-bottom: 8px
+            line-height: 18px
+            font-size: 12px
+            color: rgb(7, 17, 27)
+          .recommend
+            line-height: 16px
+            font-size: 0
+            .icon-thumb_up, .item
+              display: inline-block
+              margin: 0 8px 4px 0
+              font-size: 9px
+            .icon-thumb_up
+              color: rgb(0, 160, 220)
+            .item
+              padding: 0 6px
+              border: 1px solid rgba(7, 17, 27, .1)
+              border-radius: 1px
+              color: rgb(147, 153, 159)
+              background: #fff
+          .time
+            position: absolute
+            top: 0
+            right: 0
+            line-height: 12px
+            font-size: 10px
             color: rgb(147, 153, 159)
 </style>
